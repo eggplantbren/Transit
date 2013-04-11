c623e69b4f6addee06c06c06e6f148de692e0228
Brendon J. Brewer <brendon.brewer@gmail.com>
2013-04-12 05:13:24 +1200

    Nice display

---
+++ display.py
@@ -0,0 +1,32 @@
+from pylab import *
+from fake_data import *
+
+data = loadtxt('fake_data.txt')
+posterior_sample = loadtxt('posterior_sample.txt')
+
+ion()
+for i in xrange(0, posterior_sample.shape[0]):
+	# Noise-free transit signal
+	y = transit(data[:,0],
+			amplitude=posterior_sample[i, 0],
+			period=posterior_sample[i, 1],
+			width=posterior_sample[i, 2],
+			offset=posterior_sample[i, 3],
+			smooth=posterior_sample[i, 4])
+
+	# Plot the data and the model
+	hold(False)
+	plot(data[:,0], data[:,1], 'b.', markersize=1)
+	hold(True)
+	plot(data[:,0], y, 'r')
+
+	diff = data[:,1].max() - data[:,1].min()
+	ylim([data[:,1].min() - 0.1*diff, data[:,1].max() + 0.1*diff])
+	xlabel('Time')
+	ylabel('Flux')
+	title((i+1))
+	draw()
+
+ioff()
+show()
+
--- fake_data.py
+++ fake_data.py
@@ -20,47 +20,48 @@ def transit(t, amplitude=1., period=1., width=0.1, offset=0., smooth=0.02):
 	phase = mod(t/period - offset - 0.5, 1.) - 0.5
 	return amplitude*transit_shape(phase/width, smooth)
 
-# Reproducibility
-seed(123)
+if __name__ == '__main__':
+	# Reproducibility
+	seed(123)
 
-# Data timestamps
-t = linspace(0., 3., 100001)
+	# Data timestamps
+	t = linspace(0., 3., 100001)
 
-# Parameter values
-amplitude = 1.
-period = 1.
-width = 0.1
-offset = 0.15
-smooth = 0.02
+	# Parameter values
+	amplitude = 1.
+	period = 1.
+	width = 0.1
+	offset = 0.15
+	smooth = 0.02
 
-# Noise sd
-sigma = 1.
+	# Noise sd
+	sigma = 1.
 
-# Noise-free transit signal
-y = transit(t, amplitude=amplitude, period=period, width=width, offset=offset,
-		smooth=smooth)
+	# Noise-free transit signal
+	y = transit(t, amplitude=amplitude, period=period, width=width, offset=offset,
+			smooth=smooth)
 
-# Make noisy data
-data = empty((t.size, 3))
-data[:,0] = t
-data[:,1] = y + sigma*randn(y.size)
-data[:,2] = sigma
+	# Make noisy data
+	data = empty((t.size, 3))
+	data[:,0] = t
+	data[:,1] = y + sigma*randn(y.size)
+	data[:,2] = sigma
 
-# Save it
-savetxt('fake_data.txt', data)
+	# Save it
+	savetxt('fake_data.txt', data)
 
-# Plot the signal and the data
-subplot(2,1,1)
-plot(t, y)
-yRange = y.max() - y.min()
-ylim([y.min() - 0.1*yRange, y.max() + 0.1*yRange])
-ylabel('Flux (DC=0)')
-title('Noise-Free Signal')
+	# Plot the signal and the data
+	subplot(2,1,1)
+	plot(t, y)
+	yRange = y.max() - y.min()
+	ylim([y.min() - 0.1*yRange, y.max() + 0.1*yRange])
+	ylabel('Flux (DC=0)')
+	title('Noise-Free Signal')
 
-subplot(2,1,2)
-plot(t, data[:,1], 'b.', markersize=1)
-xlabel('$t$ (years)')
-ylabel('Flux (DC=0)')
-title('Noisy Data')
-show()
+	subplot(2,1,2)
+	plot(t, data[:,1], 'b.', markersize=1)
+	xlabel('$t$ (years)')
+	ylabel('Flux (DC=0)')
+	title('Noisy Data')
+	show()
 
